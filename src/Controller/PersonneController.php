0251c53bbbd4322d258b9f5ef44b4d67e75fe500
Nuage1985 <renaud.fontaine@protonmail.com>
2023-02-03 15:41:58 +0100

    cours symfony6 doctrine Param converters + Pagination

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -18,6 +18,30 @@ class PersonneController extends AbstractController
 
         return $this->render(view: 'personne/index.html.twig', parameters: ['personnes' => $personnes]);
     }
+
+    /*#[Route('/{id<\d+>}', name: 'personne.detail')]
+    public function detail(ManagerRegistry $doctrine, $id): Response {
+        $repository = $doctrine->getRepository(Personne::class);
+        $personne = $repository->find($id);
+
+        if(!$personne){
+            $this->addFlash(type: 'error', message: "La personne d'id $id n'existe pas.");
+            return $this->redirectToRoute(route: 'personne.list');
+        }
+
+        return $this->render(view: 'personne/detail.html.twig', parameters: ['personne' => $personne]);
+    }*/
+
+    // Avec Param converters
+    #[Route('/{id<\d+>}', name: 'personne.detail')]
+    public function detail(Personne $personne = null): Response {
+        if(!$personne){
+            $this->addFlash(type: 'error', message: "La personne n'existe pas.");
+            return $this->redirectToRoute(route: 'personne.list');
+        }
+
+        return $this->render(view: 'personne/detail.html.twig', parameters: ['personne' => $personne]);
+    }
     #[Route('/add', name: 'app_personne.add')]
     public function addPersonne(ManagerRegistry $doctrine): Response
     {
@@ -46,5 +70,25 @@ class PersonneController extends AbstractController
         return $this->render('personne/detail.html.twig', [
             'personne' => $personne,
         ]);
+    }
+
+    #[Route('/alls/{page?1}/{nbre?12}', name: 'app_personne.list.alls')]
+    public function indexAlls(ManagerRegistry $doctrine, $page, $nbre): Response {
+
+        $repository = $doctrine->getRepository(persistentObject: Personne::class);
+
+        $nbPersonne = $repository->count([]);
+        $nbPage = ceil(num: $nbPersonne / $nbre);
+        $personnes = $repository->findBy([], [], $nbre, offset: ($page-1) * $nbre);
+
+        return $this->render('personne/index.html.twig', [
+            'personnes' => $personnes,
+            'isPaginated' => true,
+            'nbrePage' => $nbPage,
+            'page' => $page,
+            'nbre' => $nbre,
+        ]);
+
+
     }
 }
